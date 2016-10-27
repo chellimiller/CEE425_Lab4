ce48167c6420faed56c38d38fe3af937b63d2db8
Michelle Miller <chellimiller@users.noreply.github.com>
2016-10-27 14:22:29 -0500

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -31,27 +31,20 @@ class TCPServer {
 			
 			
 			while(true) {
+				// Create connection socket
 				Socket cnSocket = hnSocket.accept();
 				
 				// Set up input/output streams
 				fromClient = new DataInputStream(cnSocket.getInputStream());
 				toClient = new DataOutputStream(cnSocket.getOutputStream());
 				
-				// Buffer for data input stream
+				// Create buffer and read input stream
 				int bufferLen = fromClient.available();
-				byte[] buffer = new byte[bufferLen];
-				
-				// Read new input from server		
+				byte[] buffer = new byte[bufferLen];	
 				fromClient.readFully(buffer);
 				
-				// Convert buffer to String
-				for (byte b:buffer) {
-					// Cast byte to char and add to clientIn
-					char c = (char)b;
-					clientIn = clientIn + b;
-				}
-				
-				// Print out client input
+				// Convert buffer to String and print
+				clientIn = new String(buffer.getData());				
 				System.out.println("Received: " + clientIn);
 				
 				// Convert client input to uppercase
@@ -75,28 +68,5 @@ class TCPServer {
 			cnSocket.close();
 			hnSocket.close();
 		}
-	}
-}
- 
- class TCPServer {
-	public static void main (String argv[]) throws Exception {
-		
-		// Input/output strings
-		String input;
-		String output;
-		
-		// Settings
-		int serverPortNum;
-		
-		// Create socket for handshake
-		ServerSocket hnSocket = new ServerSocket(serverPortNum);
-		
-		while(1) {
-			// Create connection socket and accept handshake
-			Socket coSocket = hnSocket.accept();
-			
-			
-		}
-		
 	}
 }
